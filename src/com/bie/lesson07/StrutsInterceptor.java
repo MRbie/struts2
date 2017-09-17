f7e1ac88ce222b0b336cb478dd0d5fd893d0dcba
MRbie <1748741328@qq.com>
2017-09-17 13:40:39 +0800

    struts 拦截器的步骤

--- src/com/bie/lesson07/StrutsInterceptor.java
+++ src/com/bie/lesson07/StrutsInterceptor.java
@@ -7,7 +7,14 @@ import com.opensymphony.xwork2.interceptor.Interceptor;
 * @author  Author:别先生 
 * @date Date:2017年9月17日 上午10:57:10 
 *
-*
+* 1:启动的时候
+* 	启动的时候创建了拦截器对象
+	执行了拦截器的初始化方法init
+  2:创建的时候
+  	2.1. Action实例创建了
+	2.2:执行action执行的一些时期，拦截器，业务处理，开始
+	2.3. 执行了请求处理的方法: execute
+	2.4:拦截器，业务处理，结束success	
 */
 public class StrutsInterceptor implements Interceptor{
 
